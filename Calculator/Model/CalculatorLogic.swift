3cfdd44f74545d7ea29a3ab867b98feb0759220b
nssina <nssina@icloud.com>
2020-03-11 10:41:48 -0400

    Build 4
    Conforming to MVC

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Sina Rabiei on 3/11/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorLogic {
+    
+}
